00090111f11c527985c0e5b1a952b4d8dbccb399
NicolasMavrides <npmavrides1@sheffield.ac.uk>
2019-01-27 12:12:32 +0000

    basic program start + rover object

--- Program.cs
+++ Program.cs
@@ -10,6 +10,26 @@ namespace Pluto_Rover
     {
         static void Main(string[] args)
         {
+            // planet grid dimensions
+            const int GRID_LENGTH = 100;
+            const int GRID_HEIGHT = 100;
+
+            // initialise a grid of zeros to represent Pluto's surface
+            int[,] planet_grid = new int[GRID_LENGTH, GRID_HEIGHT];
+
+            // position headings for North, East, South and West, whose index numbers correspond to each (eg. 0 = N)
+            String[] headings = { "N", "E", "S", "W" };
+
+            // rover object instance
+            Rover rover = new Rover(0, 0, 0);
+
+            String roverHeading = headings[rover.headingNum];
+
+            Console.WriteLine("Coordinates of rover: ");
+            Console.WriteLine(rover.xPos);
+            Console.WriteLine(rover.yPos);
+            Console.WriteLine(roverHeading);
+
         }
     }
 }
---
+++ Rover.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluto_Rover
+{
+    public class Rover
+    {
+        public int xPos { get; set; }
+        public int yPos { get; set; }
+        public int headingNum { get; set; }
+
+        public Rover(int xpos, int ypos, int headingnum)
+        {
+            xPos = xpos;
+            yPos = ypos;
+            headingNum = headingnum;
+        }
+
+    }
+}
