6899b1b68d03551f1f0ba488eb157b333f5513d9
NicolasMavrides <npmavrides1@sheffield.ac.uk>
2019-01-27 13:28:30 +0000

    Expanded handling of input for rover functionalities

--- Program.cs
+++ Program.cs
@@ -24,29 +24,42 @@ namespace Pluto_Rover
             Rover rover = new Rover(0, 0, 0);
             String roverHeading = headings[rover.headingNum];
 
+            Console.WriteLine("Enter a command: ");
             String command = Console.ReadLine();
 
             while (command != "Q")
             {
-                if (command == "F")
+                if ((command == "F" && roverHeading == "N") || (command == "B" && roverHeading == "S"))
                 {
                     rover.MoveNorth(rover);
-                    Console.Write("New rover position: "+rover.xPos + "," + rover.yPos+" and facing "+headings[rover.headingNum]);
+                    Console.WriteLine("New rover position: "+rover.xPos + "," + rover.yPos+" and facing "+ roverHeading);
                 }
-                else if (command == "B")
+                else if ((command == "B" && roverHeading == "N") || (command == "F" && roverHeading == "S"))
                 {
                     rover.MoveSouth(rover);
-                    Console.Write("New rover position: " + rover.xPos + "," + rover.yPos + " and facing " + headings[rover.headingNum]);
+                    Console.WriteLine("New rover position: " + rover.xPos + "," + rover.yPos + " and facing " + roverHeading);
+                }
+                else if ((command == "F" && roverHeading == "E") || (command == "B" && roverHeading == "W"))
+                {
+                    rover.MoveEast(rover);
+                    Console.WriteLine("New rover position: " + rover.xPos + "," + rover.yPos + " and facing " + roverHeading);
+                }
+                else if ((command == "F" && roverHeading == "W") || (command == "B" && roverHeading == "E"))
+                {
+                    rover.MoveWest(rover);
+                    Console.WriteLine("New rover position: " + rover.xPos + "," + rover.yPos + " and facing " + roverHeading);
                 }
                 else if (command == "R")
                 {
                     rover.TurnRight(rover);
-                    Console.Write("New rover position: " + rover.xPos + "," + rover.yPos + " and facing " + headings[rover.headingNum]);
+                    roverHeading = headings[rover.headingNum];
+                    Console.WriteLine("New rover position: " + rover.xPos + "," + rover.yPos + " and facing " + roverHeading);
                 }
                 else if (command == "L")
                 {
                     rover.TurnLeft(rover);
-                    Console.Write("New rover position: " + rover.xPos + "," + rover.yPos + " and facing " + headings[rover.headingNum]);
+                    roverHeading = headings[rover.headingNum];
+                    Console.WriteLine("New rover position: " + rover.xPos + "," + rover.yPos + " and facing " + roverHeading);
                 }
 
                 command = Console.ReadLine();
